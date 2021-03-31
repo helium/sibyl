4b6a8ffb60c9567cbbc03aea6f8291e8580e31a5
andymck <andrewb.mckenzie@gmail.com>
2021-03-31 16:47:25 +0100

    use updated multi service gateway proto - handle routing only

--- src/grpc/helium_routing_service.erl
+++ src/grpc/helium_routing_service.erl
@@ -1,6 +1,6 @@
--module(helium_gateway_service).
+-module(helium_routing_service).
 
--behavior(helium_gateway_bhvr).
+-behavior(helium_gateway_routing_bhvr).
 
 -include("../../include/sibyl.hrl").
 -include("autogen/server/gateway_pb.hrl").
@@ -26,9 +26,9 @@ init(_RPC, StreamState) ->
     ),
     NewStreamState.
 
--spec routing(gateway_pb:routing_request_pb(), grpcbox_stream:t()) ->
+-spec routing(gateway_pb:gateway_routing_req_v1_pb(), grpcbox_stream:t()) ->
     {ok, grpcbox_stream:t()} | grpcbox_stream:grpc_error_response().
-routing(#routing_request_pb{height = ClientHeight} = Msg, StreamState) ->
+routing(#gateway_routing_req_v1_pb{height = ClientHeight} = Msg, StreamState) ->
     lager:debug("RPC routing called with height ~p", [ClientHeight]),
     #handler_state{initialized = Initialized} = grpcbox_stream:stream_handler_state(StreamState),
     routing(Initialized, sibyl_mgr:blockchain(), Msg, StreamState).
@@ -36,17 +36,17 @@ routing(#routing_request_pb{height = ClientHeight} = Msg, StreamState) ->
 -spec routing(
     boolean(),
     blockchain:blockchain(),
-    gateway_pb:routing_request_pb(),
+    gateway_pb:gateway_routing_req_v1_pb(),
     grpcbox_stream:t()
 ) -> {ok, grpcbox_stream:t()} | grpcbox_stream:grpc_error_response().
-routing(_Initialized, undefined = _Chain, #routing_request_pb{} = _Msg, _StreamState) ->
+routing(_Initialized, undefined = _Chain, #gateway_routing_req_v1_pb{} = _Msg, _StreamState) ->
     % if chain not up we have no way to return routing data so just return a 14/503
     lager:debug("chain not ready, returning error response"),
     {grpc_error, {grpcbox_stream:code_to_status(14), <<"temporarily unavavailable">>}};
 routing(
     false = _Initialized,
     _Chain,
-    #routing_request_pb{height = ClientHeight} = _Msg,
+    #gateway_routing_req_v1_pb{height = ClientHeight} = _Msg,
     StreamState
 ) ->
     %% not previously initialized, this must be the first msg from the client
@@ -61,7 +61,7 @@ routing(
         }
     ),
     {ok, NewStreamState0};
-routing(true = _Initialized, _Chain, #routing_request_pb{} = _Msg, StreamState) ->
+routing(true = _Initialized, _Chain, #gateway_routing_req_v1_pb{} = _Msg, StreamState) ->
     %% we previously initialized, this must be a subsequent incoming msg from the client - ignore
     lager:debug("ignoring subsequent msg from client ~p", [_Msg]),
     {ok, StreamState}.
@@ -114,12 +114,16 @@ handle_event(
             [],
             ChangedKeys
         ),
-    RoutesPB = sibyl_utils:encode_routing_update_response(
-        Routes,
+
+    Msg0 = #gateway_routing_streamed_resp_v1_pb{
+        routings = [sibyl_utils:to_routing_pb(R) || R <- Routes]
+    },
+    Msg1 = sibyl_utils:encode_gateway_resp_v1(
+        Msg0,
         CurHeight,
         sibyl_mgr:sigfun()
     ),
-    NewStream = grpcbox_stream:send(false, RoutesPB, StreamState),
+    NewStream = grpcbox_stream:send(false, Msg1, StreamState),
     NewStream;
 handle_event(
     {event, _EventType, _Payload} = _Event,
@@ -128,8 +132,7 @@ handle_event(
     lager:warning("received unhandled event ~p", [_Event]),
     StreamState.
 
--spec maybe_send_inital_all_routes_msg(gateway_pb:routing_request(), grpcbox_stream:t()) ->
-    grpc:stream().
+-spec maybe_send_inital_all_routes_msg(non_neg_integer(), grpcbox_stream:t()) -> grpc:stream().
 maybe_send_inital_all_routes_msg(ClientHeight, StreamState) ->
     %% get the height field from the request msg and only return
     %% the initial full set of routes if they were modified since that height
@@ -152,12 +155,15 @@ maybe_send_inital_all_routes_msg(ClientHeight, StreamState) ->
             {ok, CurHeight} = blockchain_ledger_v1:current_height(Ledger),
             case blockchain_ledger_v1:get_routes(Ledger) of
                 {ok, Routes} ->
-                    RoutesPB = sibyl_utils:encode_routing_update_response(
-                        Routes,
+                    Msg0 = #gateway_routing_streamed_resp_v1_pb{
+                        routings = [sibyl_utils:to_routing_pb(R) || R <- Routes]
+                    },
+                    Msg1 = sibyl_utils:encode_gateway_resp_v1(
+                        Msg0,
                         CurHeight,
                         sibyl_mgr:sigfun()
                     ),
-                    NewStream = grpcbox_stream:send(false, RoutesPB, StreamState),
+                    NewStream = grpcbox_stream:send(false, Msg1, StreamState),
                     NewStream;
                 {error, _Reason} ->
                     StreamState
