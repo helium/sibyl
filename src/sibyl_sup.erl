aa2b29b7aac4ce61a2d418ab4de9373627f9f04e
andymck <andrewb.mckenzie@gmail.com>
2021-10-07 16:42:14 +0100

    only start poc_mgr if using poc over grpc

--- src/sibyl_sup.erl
+++ src/sibyl_sup.erl
@@ -7,7 +7,7 @@
 
 -behaviour(supervisor).
 
--export([start_link/0]).
+-export([start_link/0, start_link/1]).
 
 -export([init/1]).
 
@@ -47,18 +47,30 @@
 -define(SERVER, ?MODULE).
 
 start_link() ->
-    supervisor:start_link({local, ?SERVER}, ?MODULE, []).
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [grpc]).
 
-init([]) ->
+start_link(POCTransportType) ->
+    supervisor:start_link({local, ?SERVER}, ?MODULE, [POCTransportType]).
+
+init([POCTransportType]) ->
     %% create the sibyl_mgr ets table under this supervisor and set ourselves as the heir
     %% we call `ets:give_away' every time we start_link sibyl_mgr
     _ = sibyl_bus:start(),
     SibylMgrOpts = [{ets, sibyl_mgr:make_ets_table()}],
-    SibylPocMgrOpts = [],
+
+    POCServers =
+        case POCTransportType of
+            grpc ->
+                SibylPocMgrOpts = [],
+                [?WORKER(sibyl_poc_mgr, SibylPocMgrOpts)];
+            _ ->
+                []
+    end,
     {ok,
-        {?FLAGS, [
-            ?WORKER(sibyl_mgr, [SibylMgrOpts]),
-            ?WORKER(sibyl_poc_mgr, SibylPocMgrOpts)
-        ]}}.
+        {?FLAGS,
+            [
+                ?WORKER(sibyl_mgr, [SibylMgrOpts])
+            ] ++ POCServers
+        }}.
 
 %% internal functions
